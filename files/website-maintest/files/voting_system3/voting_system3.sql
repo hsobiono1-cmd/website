c3528b0ff74382313fc86f55c1c3995dcac6ee62
hsobiono1-cmd <hsobiono1@gmail.com>
2026-08-10 18:57:21 +0800

    Update voting_system3.sql

--- files/website-maintest/files/voting_system3/voting_system3.sql
+++ files/website-maintest/files/voting_system3/voting_system3.sql
@@ -68,8 +68,9 @@ CREATE TABLE IF NOT EXISTS election_status (
 INSERT INTO election_status (status) VALUES ('inactive');
 
 -- Insert default admin (password: admin123)
-INSERT INTO admins (username, password) VALUES 
-('admin', '$2y$10$sXf8Nmp15c2KdT50GTjp6eCrz.0SaFlAd2zpdoKN1LQnTXlDezR3u')
+INSERT INTO admins (username, password)
+VALUES 
+('admin', '$2y$10$sXf8Nmp15c2KdT50GTjp6eCrz.0SaFlAd2zpdoKN1LQnTXlDezR3u');
 
 -- Insert default positions
 INSERT INTO positions (position_name) VALUES 
